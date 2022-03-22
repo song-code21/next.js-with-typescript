55226239375fa3d95f40427e53c7f1f33e55a732
song-code21 <wkcjf131313@gmail.com>
2022-03-22 23:07:47 +0900

    개발환경 세팅

---
+++ components/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactElement, ReactNode } from "react"
+
+interface LayoutProps {
+  children: ReactNode;
+}
+export default function Layout({ children }: LayoutProps): ReactElement{
+  return (
+    <>
+    
+      <div>{children}</div>
+    </>
+  );
+}
